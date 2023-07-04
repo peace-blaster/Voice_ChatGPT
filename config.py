bfbf3702f994fe575f6afc5eea59ff49e4399f06
Julian DeVille <53p3n7@gmail.com>
2023-07-04 16:01:38 -0700

    sped up speech for Mac- may need to set multiple

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-SPEECH_RATE = 125 # talking speed of ChatGPT's response
+SPEECH_RATE = 150 # talking speed of ChatGPT's response
 AUDIO_LISTEN_TIME = 8 # number of seconds to listen for user input
 GPT_VERSION = "text-davinci-002" # GPT version to use
